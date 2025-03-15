94b5fd4546e10cbb63f3c48a47d52f86c0f355e4
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-03-16 01:07:46 +0200

    feat: Add script to filter waterways near groundwater sources

---
+++ filter-waterways-near-gwater.py
@@ -0,0 +1,4 @@
+waterways_file = "/mnt/data/gis/catchment/usgs-ui2/pysheds/data/waterways-trimmed.gpkg"
+waterways_layer = "waterways-trimmed"
+
+gwater_file = '/mnt/data/gis/catchment/usgs-ui2/pysheds/data/gwater-norm-3160.tif'
