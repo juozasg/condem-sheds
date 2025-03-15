28fd1d7645a75e415acd75216ee8ab216626afe6
Juozas Gaigalas <juozasgaigalas@gmail.com>
2025-03-15 18:34:44 +0200

    Refactor DEM processing workflow and remove commented-out code

--- condem.py
+++ condem.py
@@ -50,29 +50,18 @@ def condition_dem(dem_path, condem_path, eps, max_iter):
     print("condem.py: Done!")
 
 
-# Loop through multiple variations of eps and max_iter values
-# eps_values = [1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-11]
-# max_iter_values = [10, 100, 1000, 10000, 100000]
 
-# for eps in eps_values:
-#     for max_iter in max_iter_values:
-#         condem_path = f"data/condem-params/condem-ocpp-eps{eps}-iter{max_iter}.tif"
-#         d8_path = f"data/condem-params/d8-ocpp-eps{eps}-iter{max_iter}.tif"
-#         catchment_path = f"data/condem-params/catchment-ocpp-352-627-eps{eps}-iter{max_iter}.tif"
-#         accum_path = f"data/condem-params/accum-ocpp-eps{eps}-iter{max_iter}.tif"
-#         # condition_dem('data/source-ocpp.tif', condem_path, eps=eps, max_iter=max_iter)
-#         # calc_d8(condem_path, d8_path)
-#         # calc_catchment(d8_path, 352, 627, catchment_path)
-#         calc_accumulation(d8_path, accum_path)
+# LEZDOIT
+condition_dem('data/source-full.tif', 'data/condem-full.tif', eps=1e-11, max_iter=100000)
+calc_d8('data/condem-full.tif', 'data/d8-full.tif')
+calc_accumulation('data/d8-full.tif', f'data/accum-full.tif')
 
 
 
-# condition_dem('data/source-full.tif', 'data/condem-full.tif', eps=1e-11, max_iter=100000)
-# calc_d8('data/condem-full.tif', 'data/d8-full.tif')
-xi = 939
-yi = 3489
-calc_catchment('data/d8-full.tif', xi, yi, f'data/catchment-full-{xi}-{yi}.tif')
-# calc_accumulation('data/d8-full.tif', f'data/accum-full.tif')
+# xi = 939
+# yi = 3489
+# calc_catchment('data/d8-full.tif', xi, yi, f'data/catchment-full-{xi}-{yi}.tif')
+
 
 # # ARGS
 # parser = argparse.ArgumentParser(description='Condition a DEM raster')
@@ -81,6 +70,5 @@ calc_catchment('data/d8-full.tif', xi, yi, f'data/catchment-full-{xi}-{yi}.tif')
 # args = parser.parse_args()
 
 # print(args)
-
 # condition_dem(args.input_dem_path, args.output_dem_path)
 
